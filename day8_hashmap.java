8652d1ec598de2a311cfaf6b6732c72a29068392
no_word <zxcvbnm78912@gmail.com>
2018-03-26 23:17:32 +0900

    linked list deletion

---
+++ day8_hashmap.java
@@ -0,0 +1,30 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap<Object,Object> myMap = new HashMap<>();
+        
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            myMap.put(name, phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if(myMap.containsKey(s)==true)
+            {
+                System.out.println(s+"="+myMap.get(s));
+            }
+            else{
+                System.out.println("Not found");
+            }
+            
+        }
+        in.close();
+    }
+}
